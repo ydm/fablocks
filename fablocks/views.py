44d8457431251990d3f91e0dcacf8f11dca93c78
Yordan Miladinov <yordan@4web.bg>
2014-11-10 20:14:47 +0200

    more stuff

--- fablocks/views.py
+++ fablocks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.views import generic
+from fablocks import models
+
+
+class HomeView(generic.ListView):
+    model = models.Block
+    template_name = 'home.html'
+
+
+home = HomeView.as_view()
